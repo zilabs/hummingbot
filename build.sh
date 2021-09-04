3f40ae9fdb5e98abf4337f3d4908660d2ac0ef39
Long Nguyen <long@getventive.com>
2021-09-03 19:17:24 -0600

    update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 TAG=${1:-latest}
 echo "Build $TAG"
 
-docker image rm zilabs/hummingbot-sandbox:$TAG && \
+docker image rm zilabs/hummingbot-sandbox:$TAG | true
 docker build -t zilabs/hummingbot-sandbox:$TAG -f Dockerfile . && \
 docker push zilabs/hummingbot-sandbox:$TAG
